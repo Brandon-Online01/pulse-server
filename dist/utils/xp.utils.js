2326a574ed040778aa9cf048ea16c28a96baa577
Brandon-Online01 <brandonnkawu01@gmail.com>
2025-03-15 12:21:41 +0200

    ..

--- dist/utils/xp.utils.js
+++ dist/utils/xp.utils.js
@@ -2,6 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.calculateXP = calculateXP;
 function calculateXP(hours, tasks, sales) {
-    return Math.round((hours * 10) + (tasks * 5) + (sales * 0.01));
+    return Math.round(hours * 10 + tasks * 5 + sales * 0.01);
 }
 //# sourceMappingURL=xp.utils.js.map
--- src/utils/xp.utils.ts
+++ src/utils/xp.utils.ts
@@ -1,3 +1,4 @@
+//TODO: Add more complex XP calculation
 export function calculateXP(hours: number, tasks: number, sales: number): number {
-    return Math.round((hours * 10) + (tasks * 5) + (sales * 0.01));
-} 
+	return Math.round(hours * 10 + tasks * 5 + sales * 0.01);
+}
